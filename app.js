2d1ca8113c8971bc55d182b93b5b303c4d4b8a22
angelalberto <flkangel@gmail.com>
2018-01-31 10:37:38 -0300

    modifica app.js elimina linceas de codigo no necesarias

--- app.js
+++ app.js
@@ -2,17 +2,7 @@
 // se incluyen las dependencias que vamos a utilizar
 var express = require("express"); // Expresss maneja las llamadas HTTP
 var app = express();
-var bodyParser  = require("body-parser"); // Lo utilizamos para parsear los JSON
-var methodOverride = require("method-override"); //implementamos los metodos HTTP
 var MongoClient = require('mongodb').MongoClient;
-var mongoose = require('mongoose');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-
-//var router = express.Router();
 
 app.get('/', function(req, res){
     res.send("Pagina de incio");
